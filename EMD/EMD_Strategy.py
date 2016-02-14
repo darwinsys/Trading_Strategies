cb949bfbcd13499cb8a3d307ee19f837f6351faa
cocojumbo77 <jianboxue@me.com>
2016-02-14 23:32:25 +0800

    add new fiels

--- EMD/EMD_Strategy.py
+++ EMD/EMD_Strategy.py
@@ -1,24 +1,16 @@
-import odo.odo
-import blaze
-import pandas as pd
-
-'''
-from Data import StockManager
-
-dbmgr = StockManager.DBManager()
-
-mongo_uri = dbmgr.get_mongo_uri(database=dbmgr._default_mongo_database,
-                                collection=dbmgr._default_mongo_table_stock_daily_price)
-
-'''
 
 
-quandl_code = 'AuFngLLqDpLf672K9W85'
+import Data.StockDataManager as sm
+import Data.TimeSeries as ts
 
-import Quandl
-shanghai_gold = Quandl.get('CHRIS/SHFE_AU5', authtoken=quandl_code)
-price = shanghai_gold['Settle']
+settings = sm.Settings()
+tmgr = sm.TaskManager(settings)
 
-import ptsa.emd as emd
-import numpy
-imfs = emd.emd(price[1:500])
+data = ts.TimeSeries(settings).get_stock_series(['000001'], start='2012-01-01', fields=['close'])
+price = data['close']
+
+
+import pyhht.emd as emd
+import numpy as np
+imfs = emd.EMD(np.array(price))
+
